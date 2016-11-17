ab80c5749281d21081e68534a4f085abafd9d3d9
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-11-17 01:47:45 -0800

    Remove bot name before calling other services

--- slack-bot/server/intents/timeIntent.js
+++ slack-bot/server/intents/timeIntent.js
@@ -1,4 +1,5 @@
 'use strict'
+const request = require('superagent');
 module.exports.process = function (intentData, cb) {
     if (intentData.intent[0].value != 'time') {
         return cb(new Error(`Expected time intent, got ${intentData.intent[0].value}`))
@@ -7,5 +8,15 @@ module.exports.process = function (intentData, cb) {
         return cb(new Error('Missing location in time intent'));
     }
 
-    return cb(null, `TIMEINTENT: I dont yet know the time in ${intentData.location[0].value}`)
+    const location = intentData.location[0].value.replace(/,.?ndundu/i, '');
+    request
+        .get(`${process.env.TIMESERVICE_URL}:3001/service/${location}`)
+        .end((err, res) => {
+            if(err || res.statusCode != 200 || !res.body.result) {
+                console.log(err, res)
+                return cb(null, `I had a problem getting the time for ${location}.`)
+            }
+            return cb(null, `In ${location} it's now ${res.body.result}`)
+        })
+
 }
